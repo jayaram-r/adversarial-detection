d9502ad2aa8f9939dd0988eba451654995dfdeb9
Varun Chandrasekaran <chandrasekaran@durga.cs.wisc.edu>
2020-01-07 22:17:01 -0600

    updated code + checkpoints

--- expts/main.py
+++ expts/main.py
@@ -69,15 +69,16 @@ def main():
     parser = argparse.ArgumentParser(description='Arguments')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N', help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=1, metavar='N', help='number of epochs to train (default: 14)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR', help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M', help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
+    parser.add_argument('--log-interval', type=int, default=100, metavar='N', help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=True, help='For Saving the current Model')
     parser.add_argument('--model-type', default='mnist', help='model type')
     parser.add_argument('--adv-attack', default='FGSM', help='type of adversarial attack')
+    parser.add_argument('--attack', default=False, help='type of adversarial attack')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -128,7 +129,7 @@ def main():
             model = test(args, model, device, test_loader)
         scheduler.step()
 
-    if args.adv_attack:
+    if args.attack:
         if args.adv_attack == 'FGSM':
             model.to(device)
             model.eval()
