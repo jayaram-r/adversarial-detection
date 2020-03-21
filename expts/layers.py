cb3e5054b1ab01434f72deb95feb898acfae47f3
Jayaram Raghuram <jayaram@Jayarams-MacBook-Pro.local>
2020-03-21 14:51:53 -0500

    Changes to reduce memory usage

--- expts/layers.py
+++ expts/layers.py
@@ -44,8 +44,7 @@ except:
     import pickle
 
 
-def search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_test, indices_sample, model_file,
-                                   output_file, n_jobs):
+def search_dimension_and_neighbors(embeddings, labels, indices_sample, model_file, output_file, n_jobs):
     num_k_values = 10
     num_dim_values = 20
     dim_min = 50
@@ -55,9 +54,6 @@ def search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_t
     print("\nNumber of layers = {}".format(n_layers))
     # Projection model from the different layers
     model_projection_layers = []
-    # Projected (dimension reduced) training and test data from the different layers
-    # data_train_layers = []
-    # data_test_layers = []
     for i in range(n_layers):
         lines = []
         mode = 'w' if i == 0 else 'a'
@@ -68,8 +64,7 @@ def search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_t
 
         # Embeddings from layer i
         data = embeddings[i]
-        data_test = embeddings_test[i]
-        if (labels.shape[0] != data.shape[0]) or (labels_test.shape[0] != data_test.shape[0]):
+        if labels.shape[0] != data.shape[0]:
             raise ValueError("Mismatch in the size of the data and labels array!")
 
         # Random stratified sample from the training portion of the data
@@ -77,9 +72,8 @@ def search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_t
         labels_sample = labels[indices_sample]
         dim_orig = data_sample.shape[1]
         n_samples = labels_sample.shape[0]
-        str0 = ("Original dimension = {:d}. Train data size = {:d}. Test data size = {:d}. Sub-sample size used "
-                "for dimension reduction = {:d}".format(dim_orig, labels.shape[0], labels_test.shape[0],
-                                                        n_samples))
+        str0 = ("Original dimension = {:d}. Train data size = {:d}. Sub-sample size used "
+                "for dimension reduction = {:d}".format(dim_orig, labels.shape[0], n_samples))
         print(str0)
         lines.append(str0 + '\n')
 
@@ -126,10 +120,6 @@ def search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_t
         output_fp.writelines(lines)
         output_fp.close()
 
-        # print("\nProjecting the entire train and test data to {:d} dimensions:".format(dim_best))
-        # data_train_layers.append(transform_data_from_model(data, model_projection))
-        # data_test_layers.append(transform_data_from_model(data_test, model_projection))
-
     with open(model_file, 'wb') as fp:
         pickle.dump(model_projection_layers, fp)
 
@@ -137,8 +127,7 @@ def search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_t
     print("Dimension reduction models saved to the file: {}".format(model_file))
 
 
-def project_fixed_dimension(embeddings, labels, embeddings_test, labels_test, dim_proj, indices_sample,
-                            model_file, output_file, n_jobs):
+def project_fixed_dimension(embeddings, labels, dim_proj, indices_sample, model_file, output_file, n_jobs):
     n_layers = len(embeddings)
     print("\nNumber of layers = {}".format(n_layers))
     # Projection model from the different layers
@@ -151,8 +140,7 @@ def project_fixed_dimension(embeddings, labels, embeddings_test, labels_test, di
 
         # Embeddings from layer i
         data = embeddings[i]
-        data_test = embeddings_test[i]
-        if (labels.shape[0] != data.shape[0]) or (labels_test.shape[0] != data_test.shape[0]):
+        if labels.shape[0] != data.shape[0]:
             raise ValueError("Mismatch in the size of the data and labels array!")
 
         dim_orig = data.shape[1]
@@ -165,9 +153,8 @@ def project_fixed_dimension(embeddings, labels, embeddings_test, labels_test, di
             # Random stratified sample from the training portion of the data
             data_sample = data[indices_sample, :]
             labels_sample = labels[indices_sample]
-            str0 = ("Original dimension = {:d}. Train data size = {:d}. Test data size = {:d}. Sub-sample size used "
-                    "for dimension reduction = {:d}".format(dim_orig, labels.shape[0], labels_test.shape[0],
-                                                            labels_sample.shape[0]))
+            str0 = ("Original dimension = {:d}. Train data size = {:d}. Sub-sample size used for dimension reduction "
+                    "= {:d}".format(dim_orig, labels.shape[0], labels_sample.shape[0]))
             print(str0)
             lines.append(str0 + '\n')
             model_projection, _ = wrapper_data_projection(
@@ -199,10 +186,7 @@ def main():
     parser.add_argument('--fixed-dimension', '--fd', type=int, default=0,
                         help='Use this option to project the layer embeddings to a fixed dimension, if a layer '
                              'dimension exceeds this value. Zero or a negative value disables this option.')
-    parser.add_argument('--batch-size', '-b', type=int, default=64,
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', '--tb', type=int, default=1000,
-                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--batch-size', '-b', type=int, default=512, help='batch size for the data loader')
     parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
     parser.add_argument('--seed', '-s', type=int, default=1, help='random seed (default: 1)')
     parser.add_argument('--n-jobs', type=int, default=8, help='number of parallel jobs to use for multiprocessing')
@@ -239,7 +223,7 @@ def main():
         )
         test_loader = torch.utils.data.DataLoader(
             datasets.MNIST(data_path, train=False, download=True, transform=transform),
-            batch_size=args.test_batch_size, shuffle=True, **kwargs
+            batch_size=args.batch_size, shuffle=True, **kwargs
         )
         model = MNIST().to(device)
         num_classes = 10
@@ -252,7 +236,7 @@ def main():
         trainset = datasets.CIFAR10(root=data_path, train=True, download=True, transform=transform)
         train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, **kwargs)
         testset = datasets.CIFAR10(root=data_path, train=False, download=True, transform=transform)
-        test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=True, **kwargs)
+        test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=True, **kwargs)
         model = ResNet34().to(device)
         num_classes = 10
     
@@ -264,7 +248,7 @@ def main():
         trainset = datasets.SVHN(root=data_path, split='train', download=True, transform=transform)
         train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, **kwargs)
         testset = datasets.SVHN(root=data_path, split='test', download=True, transform=transform)
-        test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=True, **kwargs)
+        test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=True, **kwargs)
         model = SVHN().to(device)
         num_classes = 10
     
@@ -281,7 +265,7 @@ def main():
         model, device, train_loader, method=args.detection_method
     )
     print("\nCalculating layer embeddings for the test data:")
-    embeddings_test, labels_test, labels_pred_test, counts_test = extract_layer_embeddings(
+    _, labels_test, labels_pred_test, counts_test = extract_layer_embeddings(
         model, device, test_loader, method=args.detection_method
     )
     accu_test = np.sum(labels_test == labels_pred_test) / float(labels_test.shape[0])
@@ -301,8 +285,8 @@ def main():
         output_file = os.path.join(output_dir, 'output_layer_extraction.txt')
         model_file = os.path.join(output_dir, 'models_dimension_reduction.pkl')
         # Search for the best number of dimensions and number of neighbors and save the corresponding projection model
-        search_dimension_and_neighbors(embeddings, labels, embeddings_test, labels_test, indices_sample, model_file,
-                                       output_file, n_jobs)
+        search_dimension_and_neighbors(embeddings, labels, indices_sample, model_file, output_file, n_jobs)
+
     else:
         if args.detection_method in ['lid', 'lid_class_cond']:
             # This method uses a different (larger) number of layer embeddings
@@ -313,8 +297,8 @@ def main():
             model_file = os.path.join(output_dir, "models_fixed_dimension_{:d}.pkl".format(args.fixed_dimension))
 
         # Project the embeddings from each layer to the specified fixed dimension, if it exceeds the fixed dimension
-        project_fixed_dimension(embeddings, labels, embeddings_test, labels_test, args.fixed_dimension,
-                                indices_sample, model_file, output_file, n_jobs)
+        project_fixed_dimension(embeddings, labels, args.fixed_dimension, indices_sample, model_file,
+                                output_file, n_jobs)
 
 
 if __name__ == '__main__':
