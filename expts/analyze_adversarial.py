3a4c7e45967cd33f4cf519ef4db61e7c66853c9b
Jayaram Raghuram <jayaram@Jayarams-MacBook-Pro.local>
2020-05-09 19:40:14 -0500

    Changes to allow analysis of only the test fold data

--- expts/analyze_adversarial.py
+++ expts/analyze_adversarial.py
@@ -13,49 +13,71 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def analyze_adversarial_samples(data_path, output_path, norm_type='inf'):
-    # Perturbed inputs
-    data_tr_adv = np.load(os.path.join(data_path, "data_tr_adv.npy"))
-    data_te_adv = np.load(os.path.join(data_path, "data_te_adv.npy"))
-    # Clean inputs
-    data_tr_clean = np.load(os.path.join(data_path, "data_tr_clean.npy"))
-    data_te_clean = np.load(os.path.join(data_path, "data_te_clean.npy"))
-    # Predicted (mis-classified) labels
-    labels_pred_tr = np.load(os.path.join(data_path, "labels_tr_adv.npy"))
-    labels_pred_te = np.load(os.path.join(data_path, "labels_te_adv.npy"))
-    # Labels of the original inputs from which the adversarial inputs were created
-    labels_tr = np.load(os.path.join(data_path, "labels_tr_clean.npy"))
-    labels_te = np.load(os.path.join(data_path, "labels_te_clean.npy"))
+def analyze_adversarial_samples(data_path, output_path, norm_type='inf', train_only=False, test_only=False):
+    if train_only and test_only:
+        raise ValueError("Both inputs 'train_only' and 'test_only' cannot be set to True")
 
-    # Check if the original and adversarial labels are all different
-    check_label_mismatch(labels_tr, labels_pred_tr)
-    check_label_mismatch(labels_te, labels_pred_te)
+    norm_diff_list = []
+    if not test_only:
+        # Perturbed inputs
+        data_tr_adv = np.load(os.path.join(data_path, "data_tr_adv.npy"))
+        # Clean inputs
+        data_tr_clean = np.load(os.path.join(data_path, "data_tr_clean.npy"))
+        # Predicted (mis-classified) labels
+        labels_pred_tr = np.load(os.path.join(data_path, "labels_tr_adv.npy"))
+        # Labels of the original inputs from which the adversarial inputs were created
+        labels_tr = np.load(os.path.join(data_path, "labels_tr_clean.npy"))
 
-    # Flatten all but the first dimension and take the vector norm of each row in `diff`
-    n_train = data_tr_clean.shape[0]
-    diff = data_tr_adv.reshape(n_train, -1) - data_tr_clean.reshape(n_train, -1)
-    if norm_type == 'inf':
-        norm_diff_tr = np.linalg.norm(diff, ord=np.inf, axis=1)
-    else:
-        # expecting a non-negative integer
-        norm_diff_tr = np.linalg.norm(diff, ord=int(norm_type), axis=1)
+        # Flatten all but the first dimension and take the vector norm of each row in `diff`
+        n_train = data_tr_clean.shape[0]
+        diff = data_tr_adv.reshape(n_train, -1) - data_tr_clean.reshape(n_train, -1)
+        if norm_type == 'inf':
+            norm_diff_tr = np.linalg.norm(diff, ord=np.inf, axis=1)
+        else:
+            # expecting a non-negative integer
+            norm_diff_tr = np.linalg.norm(diff, ord=int(norm_type), axis=1)
 
-    n_test = data_te_clean.shape[0]
-    diff = data_te_adv.reshape(n_test, -1) - data_te_clean.reshape(n_test, -1)
-    if norm_type == 'inf':
-        norm_diff_te = np.linalg.norm(diff, ord=np.inf, axis=1)
-    else:
-        # expecting a non-negative integer
-        norm_diff_te = np.linalg.norm(diff, ord=int(norm_type), axis=1)
+        mask_adv = norm_diff_tr > 1e-16
+        norm_diff_list.append(norm_diff_tr[mask_adv])
+        # Check if the original and adversarial labels are all different
+        check_label_mismatch(labels_tr[mask_adv], labels_pred_tr[mask_adv])
+        print("Train fold: number of samples = {:d}, number of adversarial samples = {:d}".
+              format(mask_adv.shape[0], mask_adv[mask_adv].shape[0]))
+
+    if not train_only:
+        # Perturbed inputs
+        data_te_adv = np.load(os.path.join(data_path, "data_te_adv.npy"))
+        # Clean inputs
+        data_te_clean = np.load(os.path.join(data_path, "data_te_clean.npy"))
+        # Predicted (mis-classified) labels
+        labels_pred_te = np.load(os.path.join(data_path, "labels_te_adv.npy"))
+        # Labels of the original inputs from which the adversarial inputs were created
+        labels_te = np.load(os.path.join(data_path, "labels_te_clean.npy"))
+
+        # Flatten all but the first dimension and take the vector norm of each row in `diff`
+        n_test = data_te_clean.shape[0]
+        diff = data_te_adv.reshape(n_test, -1) - data_te_clean.reshape(n_test, -1)
+        if norm_type == 'inf':
+            norm_diff_te = np.linalg.norm(diff, ord=np.inf, axis=1)
+        else:
+            # expecting a non-negative integer
+            norm_diff_te = np.linalg.norm(diff, ord=int(norm_type), axis=1)
+
+        mask_adv = norm_diff_te > 1e-16
+        norm_diff_list.append(norm_diff_te[mask_adv])
+        # Check if the original and adversarial labels are all different
+        check_label_mismatch(labels_te[mask_adv], labels_pred_te[mask_adv])
+        print("Test fold: number of samples = {:d}, number of adversarial samples = {:d}".
+              format(mask_adv.shape[0], mask_adv[mask_adv].shape[0]))
 
     if not os.path.isdir(output_path):
         os.makedirs(output_path)
 
-    norm_diff = np.concatenate([norm_diff_tr, norm_diff_te])
+    norm_diff = np.concatenate(norm_diff_list)
     perc = [0, 1, 25, 50, 75, 99, 100]
     lines = ['{}\t{}\n'.format('perc', 'value')]
     v = np.percentile(norm_diff, perc)
-    print("\nTrain fold. Number of samples = {:d}".format(n_train + n_test))
+    print("\nNumber of adversarial samples = {:d}".format(norm_diff.shape[0]))
     print("Percentile of {}-norm values:".format(norm_type))
     for a, b in zip(perc, v):
         print("{:2d}\t{:.8f}".format(a, b))
@@ -89,13 +111,18 @@ def main():
                                                                  'adversarial attack')
     parser.add_argument('--norm', '-n', default='inf', help="Norm type to use. For example 'inf', '1', '2', '0'")
     parser.add_argument('--output-dir', '-o', default='', help='directory for output files')
+    parser.add_argument('--test-only', action='store_true', default=False,
+                        help='Use option to analyze only the test fold data')
+    parser.add_argument('--train-only', action='store_true', default=False,
+                        help='Use option to analyze only the train fold data')
     args = parser.parse_args()
 
     output_dir = args.output_dir
     if not output_dir:
         output_dir = os.path.join(os.getcwd(), 'norm_analysis')
 
-    analyze_adversarial_samples(args.data_path, output_dir, norm_type=args.norm)
+    analyze_adversarial_samples(args.data_path, output_dir, norm_type=args.norm, train_only=args.train_only,
+                                test_only=args.test_only)
 
 
 if __name__ == '__main__':
