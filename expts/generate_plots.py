80c529fb4702c1efc2ed1e2082bba0b80f68e84b
Jayaram Raghuram <jayaram@Jayarams-MacBook-Pro.local>
2020-05-23 09:03:08 -0500

    Specify a plot directory

--- expts/generate_plots.py
+++ expts/generate_plots.py
@@ -12,6 +12,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output-dir', '-o', required=True, help='output directory with the performance '
                                                                   'metrics files')
+    parser.add_argument('--plot-dir', '-o', default='', help='directory to save the plot files')
     parser.add_argument('--pos-label', '-p', default='adversarial',
                         help='label for the positive class - e.g. adversarial or ood')
     parser.add_argument('--name-prefix', '--pre', default='', help='optional prefix for the plot filenames')
@@ -33,13 +34,19 @@ def main():
         with open(fname, 'rb') as fp:
             results[m] = pickle.load(fp)
 
+    if args.plot_dir:
+        plot_dir = args.plot_dir
+    else:
+        plot_dir = args.output_dir
+
+    # legend inside or outside the plot
     if len(methods) <= 8:
         plo = False
     else:
         plo = True
 
     if results:
-        plot_performance_comparison(results, args.output_dir, place_legend_outside=plo, pos_label=args.pos_label,
+        plot_performance_comparison(results, plot_dir, place_legend_outside=plo, pos_label=args.pos_label,
                                     log_scale=args.log_scale, hide_errorbar=True, name_prefix=args.name_prefix)
     else:
         print("No performance metrics files were found in the specified output directory.")
